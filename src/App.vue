a5eb1430027237c8ada20977b3b3d2bb44340121
Francesco Vincenzi <francesco.vincenzi@edu.unito.it>
2024-09-26 21:11:35 +0200

    rimosso codice che dava errori

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,10 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link>
+    <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
   <router-view />
 </template>
-
-<template>
-  <div id="app">
-    <Slidercomponent />
-  </div>
-</template>
-
-<script>
-import Slidercomponent from "./components/Slidercomponent.vue";
-
-export default {
-  name: "App",
-  components: {
-    Slidercomponent,
-  },
-};
-</script>
 
 <style>
 #app {
